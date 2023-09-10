4f4a62ad457ab7c50012bfa7ed2b5b1eb68f9458
cyrdam <51902761-cyrdam@users.noreply.gitlabgithub.com>
2023-09-10 19:07:31 +0200

    chore: added eslint-config shared package

--- packages/vite-react-starter-2/.eslintrc.cjs
+++ packages/vite-react-starter-2/.eslintrc.cjs
@@ -2,12 +2,10 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    '@shared/eslint-config',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
